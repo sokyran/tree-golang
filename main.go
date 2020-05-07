db6ffed30ef2cfd948e01cf08331b3c00bb501db
sokyran <karass0001@gmail.com>
2020-05-07 17:17:22 +0300

    Fixed graphical output. Added some comments.

--- main.go
+++ main.go
@@ -6,25 +6,33 @@ import (
 	"io/ioutil"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
-// myFolder : myFolder describes folder and contains it's path, counter of folders being last, level of depth, files list, and parent folder
+// myFolder : myFolder describes folder and contains it's path, counter of folders being last, files list, and it's identation string
 type myFolder struct {
 	path        string
 	lastCount   int
-	level       int
-	prev        *myFolder
 	files       map[string]bool
 	levelSymbol string
 }
 
+// Get filename and IsDir from os.FileInfo and create map from those values. Then set this map to 'files' variable of myFolder type
 func (f *myFolder) setFiles(filesInfo []os.FileInfo) {
 	newFiles := make(map[string]bool, 0)
 	for _, file := range filesInfo {
 		newFiles[file.Name()] = file.IsDir()
 	}
 	f.files = newFiles
+}
+
+// Reverse string
+func reverse(s string) (result string) {
+	for _, v := range s {
+		result = string(v) + result
+	}
+	return
 }
 
 func main() {
@@ -50,7 +58,7 @@ func dirTree(out io.Writer, path string, printFiles bool) error {
 	}
 
 	if printFiles {
-		err := showTreeNoFiles(out, startFolder)
+		err := showTreeWithFiles(out, startFolder)
 		if err != nil {
 			return fmt.Errorf(err.Error())
 		}
@@ -63,12 +71,15 @@ func dirTree(out io.Writer, path string, printFiles bool) error {
 	return nil
 }
 
+// List folders without files
 func showTreeNoFiles(out io.Writer, Folder myFolder) error {
+	// Essential variables
 	fileNotLastSymbol := "├───"
 	fileLastSymbol := "└───"
 	files := Folder.files
 	lastCount := Folder.lastCount
 
+	// Get all folders and sort them
 	folders := make([]string, 0)
 	for file, isDir := range files {
 		if isDir {
@@ -83,11 +94,77 @@ func showTreeNoFiles(out io.Writer, Folder myFolder) error {
 		fileLevel := strings.Count(fullPath, string(os.PathSeparator))
 
 		beforeFile := ""
+		// So, if folder is last, then we need to delete 1 '|' symbol so that's why we store count of last folders
 		if folderIsLast {
 			beforeFile = fileLastSymbol
 			lastCount++
 		} else {
 			beforeFile = fileNotLastSymbol
+		}
+
+		// If this is not starting foder, then we add '|' before then name
+		levelSymbol := ""
+		if Folder.levelSymbol == "" {
+			for i := 1; i < fileLevel; i++ {
+				levelSymbol += "│\t"
+			}
+		} else {
+			levelSymbol = Folder.levelSymbol + "│\t"
+		}
+
+		// If files are in last folder, then we need to delete some of the '|'
+		if Folder.lastCount > 0 {
+			for i := 0; i < Folder.lastCount; i++ {
+				parentCount := strings.Count(Folder.levelSymbol, "│")
+				childCount := strings.Count(levelSymbol, "│")
+				if parentCount == childCount {
+					break
+				}
+				levelReversed := reverse(levelSymbol)
+				levelReversed = strings.Replace(levelReversed, "│", "", 1)
+				levelSymbol = reverse(levelReversed)
+			}
+		}
+
+		if !folderIsLast {
+			lastCount = 0
+		}
+		fmt.Fprintf(out, "%v%v%v\n", levelSymbol, beforeFile, folder)
+
+		allFiles, _ := ioutil.ReadDir(fullPath)
+		newFolder := myFolder{path: fullPath, lastCount: lastCount, levelSymbol: levelSymbol}
+		newFolder.setFiles(allFiles)
+
+		showTreeNoFiles(out, newFolder)
+	}
+	return nil
+}
+
+// List all folders with files and with their size
+func showTreeWithFiles(out io.Writer, Folder myFolder) error {
+	fileNotLastSymbol := "├───"
+	fileLastSymbol := "└───"
+	files := Folder.files
+	lastCount := Folder.lastCount
+
+	allFiles := make([]string, 0)
+	for file := range files {
+		allFiles = append(allFiles, file)
+	}
+	sort.Strings(allFiles)
+
+	for idx, file := range allFiles {
+		fullPath := Folder.path + string(os.PathSeparator) + file
+		fileIsLast := idx == len(allFiles)-1
+		fileLevel := strings.Count(fullPath, string(os.PathSeparator))
+
+		beforeFile := ""
+		if fileIsLast {
+			beforeFile = fileLastSymbol
+			lastCount++
+		} else {
+			beforeFile = fileNotLastSymbol
+			lastCount = 0
 		}
 		levelSymbol := ""
 		if Folder.levelSymbol == "" {
@@ -111,95 +188,24 @@ func showTreeNoFiles(out io.Writer, Folder myFolder) error {
 			}
 		}
 
-		if !folderIsLast {
-			lastCount = 0
+		if isDir := files[file]; isDir {
+			fmt.Fprintf(out, "%v%v%v\n", levelSymbol, beforeFile, file)
+			allFiles, _ := ioutil.ReadDir(fullPath)
+			newFolder := myFolder{path: fullPath, lastCount: lastCount, levelSymbol: levelSymbol}
+			newFolder.setFiles(allFiles)
+			showTreeWithFiles(out, newFolder)
+		} else {
+			fileStats, _ := os.Stat(fullPath)
+			size := fileStats.Size()
+			sizeStr := ""
+			if size == 0 {
+				sizeStr = "empty"
+			} else {
+				sizeStr = strconv.FormatInt(size, 10) + "b"
+			}
+			fmt.Fprintf(out, "%v%v%v (%v)\n", levelSymbol, beforeFile, file, sizeStr)
 		}
-		fmt.Fprintf(out, "%v%v%v \n", levelSymbol, beforeFile, folder)
 
-		allFiles, _ := ioutil.ReadDir(fullPath)
-		newFolder := myFolder{path: fullPath, lastCount: lastCount, level: fileLevel, prev: &Folder, levelSymbol: levelSymbol}
-		newFolder.setFiles(allFiles)
-
-		showTreeNoFiles(out, newFolder)
 	}
 	return nil
-}
-
-// func showTreeWithFiles(out io.Writer, folder myFolder) error {
-// 	fileNotLastSymbol := "├───"
-// 	fileLastSymbol := "└───"
-// 	files := folder.files
-// 	usualFiles := make([]string, 0)
-// 	for _, file := range files {
-// 		if file.IsDir() {
-// 			folders = append(folders, file.Name())
-// 		} else {
-// 			usualFiles = append(usualFiles, file.Name())
-// 		}
-// 	}
-
-// 	sort.Strings(usualFiles)
-// 	folders = append(folders, usualFiles...)
-// 	sort.Strings(folders)
-
-// 	for idx, folder := range folders {
-// 		fullPath := path + string(os.PathSeparator) + folder
-// 		fileIsLast := idx == len(folders)-1
-// 		fileLevel := strings.Count(fullPath, string(os.PathSeparator))
-
-// 		beforeFile := ""
-// 		if fileIsLast {
-// 			beforeFile = fileLastSymbol
-// 		} else {
-// 			beforeFile = fileNotLastSymbol
-// 		}
-
-// 		levelSymbol := ""
-// 		for i := 1; i < fileLevel; i++ {
-// 			levelSymbol += "│\t"
-// 		}
-
-// 		if lastFolderCount > 0 {
-// 			levelReversed := reverse(levelSymbol)
-// 			levelReversed = strings.Replace(levelReversed, "│", "", lastFolderCount)
-// 			levelSymbol = reverse(levelReversed)
-// 		}
-
-// 		if !find(usualFiles, folder) {
-// 			if fileIsLast {
-// 				lastFolderCount++
-// 			} else {
-// 				lastFolderCount = 0
-// 			}
-// 		}
-
-// 		if foundInFiles := find(usualFiles, folder); foundInFiles {
-// 			fileInfo, _ := os.Stat(fullPath)
-// 			size := strconv.FormatInt(fileInfo.Size(), 10) + "b"
-// 			if size == "0b" {
-// 				size = "empty"
-// 			}
-// 			fmt.Fprintf(out, "%v%v%v (%v)\n", levelSymbol, beforeFile, folder, size)
-// 		} else {
-// 			fmt.Fprintf(out, "%v%v%v\n", levelSymbol, beforeFile, folder)
-// 		}
-// 		showTreeWithFiles(out, fullPath, lastFolderCount)
-// 	}
-// 	return nil
-// }
-
-func find(list []string, a string) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
-}
-
-func reverse(s string) (result string) {
-	for _, v := range s {
-		result = string(v) + result
-	}
-	return
 }
